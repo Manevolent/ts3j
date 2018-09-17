4c539e44c7d55a7d17a39121de73ac0d2bfac9d0
Manevolent <jproctor796@gmail.com>
2018-09-17 19:51:33 -0400

    Proper extension

--- src/main/java/com/github/manevolent/ts3j/protocol/socket/server/TeamspeakServerSocket.java
+++ src/main/java/com/github/manevolent/ts3j/protocol/socket/server/TeamspeakServerSocket.java
@@ -1,8 +1,9 @@
 package com.github.manevolent.ts3j.protocol.socket.server;
 
+import com.github.manevolent.ts3j.protocol.socket.TeamspeakSocket;
 import com.github.manevolent.ts3j.protocol.socket.client.TeamspeakClientSocket;
 
-public interface TeamspeakServerSocket {
+public interface TeamspeakServerSocket extends TeamspeakSocket {
 
     TeamspeakClientSocket accept();
 
